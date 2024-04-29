4691d2f0cb3a0ca1fa389a5e2d399ed2d0bc2e35
Aman Yadav <113305877+AmanYadav96@users.noreply.github.com>
2024-04-29 13:55:46 +0530

    payment link

--- StartBusiness/cart/urls.py
+++ StartBusiness/cart/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('view/<uuid:input>/',CartViewById.as_view(), name = 'Cart views single'),
     path('update/<uuid:input>/',CartUpdateView.as_view(), name = 'Cart update '),
     path('delete/<uuid:input>',CartDeleteView.as_view(), name = 'Cart delete'),
+    path('payment',paymentView.as_view(), name = 'Cart register'),
+    path('callback',paymentView.as_view(), name = 'Cart register'),
 
 ]
