3b126dbf5c96ff7e5cc0266a7dd61787fdc89494
Aman Yadav <113305877+AmanYadav96@users.noreply.github.com>
2024-05-08 15:18:21 +0530

    email done

--- StartBusiness/user/views.py
+++ StartBusiness/user/views.py
@@ -36,7 +36,7 @@ def otp_generator(id):
    user = User.objects.get(user_email=id)
    user.otp_key = d
    user.save()
-   send_verification_email(otp, id)
+   send_verification_email.delay(otp, id)
   
 
 
