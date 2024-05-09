eae79e4488214e258ad19c10aa1ba841aabb93de
Aman Yadav <113305877+AmanYadav96@users.noreply.github.com>
2024-05-09 11:32:21 +0530

    mistake

--- StartBusiness/compare/urls.py
+++ StartBusiness/compare/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('add/.',AddToCompareView.as_view(), name = 'AddToCompare'),
+    path('add/',AddToCompareView.as_view(), name = 'AddToCompare'),
     path('delete/<uuid:input>/',CompareDeleteView.as_view(),name='update compare item'),
     path('view/<uuid:input>/',CompareView.as_view(), name = 'AddToCompare')
     
