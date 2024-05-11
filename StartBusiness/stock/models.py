3830aa39e49c9787b9ab0752130993edd9c5971f
Kuldeep Yadav <kuldeepy.bca2022@ssism.org>
2024-05-11 15:57:59 +0530

    create stock add api

--- StartBusiness/stock/models.py
+++ StartBusiness/stock/models.py
@@ -1,3 +1,5 @@
+import random
+import string
 from django.db import models
 import uuid
 from category.models import Category
@@ -7,8 +9,6 @@ from product.models import Product
 
 class Stock(models.Model):
     stock_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
-    product_id = models.OneToOneField(Product, on_delete=models.CASCADE,default=uuid.uuid4)
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE,default=uuid.uuid4) 
     current_stock = models.PositiveBigIntegerField(blank=True,null=True)
     unit = models.PositiveBigIntegerField(blank=True,null=True)
     default_price = models.FloatField(blank=True,null=True)
@@ -16,6 +16,16 @@ class Stock(models.Model):
     regular_buying_price = models.FloatField(blank=True,null=True)
     mrp = models.FloatField(blank=True,null=True)
     stock_status = models.BooleanField(default=False)
+    revenue = models.PositiveBigIntegerField(blank=True,null=True)
+    sales = models.PositiveBigIntegerField(blank=True,null=True)
+    minimum_stock_level = models.PositiveBigIntegerField(blank=True,null=True)
+    maximum_stock_level = models.PositiveBigIntegerField(blank=True,null=True)
+    dealer_price = models.PositiveBigIntegerField(blank=True,null=True)
+    hsn_code = models.CharField(max_length=8,default=''.join(random.choices(string.ascii_uppercase, k=3))+''.join(random.choices(string.digits, k=5)),editable=False)
+    product_id = models.OneToOneField(Product, on_delete=models.CASCADE,default=uuid.uuid4)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE,default=uuid.uuid4) 
+
+
     
 
 
