bb894ae526023327a7c20de67203d3f108eac960
Krishna Sadhu <Krishnas.bca2022@ssism.org>
2024-05-15 14:54:35 +0530

    create payment api

--- StartBusiness/payment/models.py
+++ StartBusiness/payment/models.py
@@ -1,5 +1,16 @@
 from django.db import models
-
-
+import uuid
+from user.models import User
+from order.models import Order
 
 # Create your models here.
+class Payment(models.Model):
+    payment_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    payment_method = models.CharField(max_length=50)
+    transaction_id = models.CharField(max_length=100, unique=True)
+    status = models.CharField(max_length=20, default='pending')
+    timestamp_initiated = models.DateTimeField(auto_now_add=True)
+    timestamp_completed = models.DateTimeField(null=True, blank=True)
--- StartBusiness/payment/views.py
+++ StartBusiness/payment/views.py
@@ -1,3 +1,96 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from payment.models import Payment
+from rest_framework.generics import GenericAPIView
+from payment.serializers import PaymentSerializer
+# Create your views here.
+
+class PaymentAddView(GenericAPIView):
+        serializer_class = PaymentSerializer
+        def post(self, request,format=None):
+          serializer = PaymentSerializer(data=request.data)
+          serializer.is_valid(raise_exception=True)
+          serializer.save()
+          return Response({
+              'status':status.HTTP_201_CREATED,
+              "msg":'Payment Created successfully',
+              'data':serializer.data,
+          },status=201)
+
+
+class PaymentView(APIView):
+    serializer_class = PaymentSerializer
+    def get(self, request, input=None, format=None):
+        _id = input
+        print(_id)
+        if _id is not None:
+            try:
+                payment  = Payment.objects.get(payment_id=_id)
+                serializer = PaymentSerializer(payment)
+                return Response(
+                    {
+                        'status': status.HTTP_200_OK,
+                        'message': 'Payment data retrieved successfully',
+                        'data': serializer.data,
+                    }, status=200
+                )
+            except Payment.DoesNotExist:
+                return Response(
+                    {
+                        'status':  status.HTTP_404_NOT_FOUND,
+                        'message': "invalid payment_id",
+                    },
+                    status=404
+                )
+        else:
+            payment = Payment.objects.all()
+            serializer = PaymentSerializer(payment, many=True)
+            return Response({
+                 'status': status.HTTP_200_OK,
+                 'message': 'Payment data retrieved successfully',
+                 'data': serializer.data,
+            }, status=200)
+        
+
+class PaymentUpdateView(GenericAPIView):
+    serializer_class = PaymentSerializer
+    def patch(self, request, input):
+        _id = input
+        try:
+           payment = Payment.objects.get(payment_id=_id)
+           serializer = PaymentSerializer(payment, data=request.data, partial=True)
+           serializer.is_valid(raise_exception=True)
+           serializer.save()
+           return Response({
+                'status': status.HTTP_200_OK,
+                'message': 'Payment Updated Successfully',
+                'data': serializer.data
+                },status=200)
+        except payment.DoesNotExist:
+            return Response({
+               'status': status.HTTP_404_NOT_FOUND,
+                'message': 'invalid payment_id',
+                },
+                status=404)
+
+class PaymentDeleteView(APIView):
+    def delete(self, request, input):
+        try:
+            _id = input
+            order = Payment.objects.get(payment_id=_id)
+            order.delete()
+            return Response({
+                'status': status.HTTP_200_OK,
+                'message': 'Payment Deleted Successfully'
+            },status=200)
+        except Payment.DoesNotExist:
+            return Response({
+                'status': status.HTTP_404_NOT_FOUND,
+                'message': 'Invalid Payment_id'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+
 # Create your views here.
