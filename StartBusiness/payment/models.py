1b22e0ce02743fbd97820dc18051a9aa631274fd
Kuldeep Yadav <kuldeepy.bca2022@ssism.org>
2024-05-15 15:44:55 +0530

    update order api in order app

--- StartBusiness/payment/models.py
+++ StartBusiness/payment/models.py
@@ -1,5 +1,11 @@
+import uuid
 from django.db import models
 
 
 
-# Create your models here.
+class Payment(models.Model):
+    payment_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    payment_method = models.CharField(max_length = 50, choices=(('UPI', 'UPI'),('CREDIT CARD', 'CREDIT CARD'),('DEBIT CARD', 'DEBIT CARD'),('NET BANKING', 'NET BANKING')),default='')
+    payment_status = models.CharField(max_length = 50, choices=(('Paid', 'Paid'),('Unpaid', 'Unpaid')),default='Unpaid',editable=False)
+    payment_date = models.DateTimeField(auto_now_add=True,editable=False)
+    
