0ff4e66ea214255a2746e01641730f3ddc76c766
Kuldeep Yadav <kuldeepy.bca2022@ssism.org>
2024-05-17 15:03:00 +0530

    changes in order api

--- StartBusiness/order/models.py
+++ StartBusiness/order/models.py
@@ -9,16 +9,16 @@ class Order(models.Model):
     order_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     order_status = models.CharField(max_length = 50, choices=(('Placed', 'Placed'),('Pending', 'Pending'),('Cancelled','Cancelled')),default='Placed',editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.PositiveBigIntegerField(default=0)
     user = models.ForeignKey(User,default=uuid.uuid4,on_delete=models.CASCADE)
     address = models.ForeignKey(Address,default=uuid.uuid4,on_delete=models.CASCADE)
 
 
 class OrderItem(models.Model):
     order_item_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
-    order = models.ForeignKey(Order,default=uuid.uuid4,on_delete=models.CASCADE)
+    order = models.ForeignKey(Order,related_name='items',default=uuid.uuid4,on_delete=models.CASCADE)
     product = models.ForeignKey(Product,default=uuid.uuid4,on_delete=models.CASCADE)
-    price = models.PositiveBigIntegerField(default=0,editable=False)
-    discount_price = models.PositiveBigIntegerField(default=0,editable=False)
-    delivery_charges = models.PositiveIntegerField(default=0)
-    total_price = models.PositiveBigIntegerField(default=0,editable=False)
     quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveBigIntegerField(default=0)
+    discount_price = models.PositiveBigIntegerField(default=0)
+    delivery_charges = models.PositiveIntegerField(default=0)
--- StartBusiness/order/serializers.py
+++ StartBusiness/order/serializers.py
@@ -1,20 +1,23 @@
 from rest_framework import serializers
 from .models import OrderItem,Order
+from django.db import transaction
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = '__all__'
+
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
 
     class Meta:
         model = Order
-        fields = ['user', 'address','items']
+        fields = ['user', 'address','items','total_price']
 
     def create(self, validated_data):
         items_data = validated_data.pop('items')
-        order = Order.objects.create(**validated_data)
-        for item_data in items_data:
-            OrderItem.objects.create(order=order, **item_data)
+        with transaction.atomic():
+            order = Order.objects.create(**validated_data)
+            order_items = [OrderItem(order=order, **item_data) for item_data in items_data]
+            OrderItem.objects.bulk_create(order_items)
         return order
