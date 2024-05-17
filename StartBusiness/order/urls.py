eec3f08718558075cbd4e0fbcda314bba66472f2
Kuldeep Yadav <kuldeepy.bca2022@ssism.org>
2024-05-17 10:40:42 +0530

    some changes in order app

--- StartBusiness/order/urls.py
+++ StartBusiness/order/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('add',OrderAddView.as_view(), name = 'Order add'),
     path('view/',OrderView.as_view(), name = 'Order views'),
     path('view/<uuid:input>/',OrderView.as_view(), name = 'Order views single'),
-    path('update/<uuid:input>/',OrderItemUpdateView.as_view(), name = 'Order update '),
-    path('deleteItem/<uuid:input>',OrderDeleteView.as_view(), name = 'Order delete'),
-    path('addItem',OrderItemAddView.as_view(), name = 'Order_item add'),
+    # path('update/<uuid:input>/',OrderItemUpdateView.as_view(), name = 'Order update '),
+    # path('deleteItem/<uuid:input>',OrderDeleteView.as_view(), name = 'Order delete'),
+    # path('addItem',OrderItemAddView.as_view(), name = 'Order_item add'),
 
 ]
--- StartBusiness/order/views.py
+++ StartBusiness/order/views.py
@@ -2,8 +2,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
-from order.models import Order,OrderItem
-from order.serializers import OrderSerializer,OrderItemSerializer
+from order.models import Order
+from order.serializers import OrderSerializer
 
 # add Order
 class OrderAddView(GenericAPIView):
@@ -22,26 +22,26 @@ class OrderAddView(GenericAPIView):
         },status=201)
 
 
-# update Order
-class OrderItemUpdateView(GenericAPIView):
-    serializer_class = OrderItemSerializer
-    def patch(self, request, input):
-        _id = input
-        try:
-           order_item = OrderItem.objects.get(order_item_id=_id)
-           serializer = OrderSerializer(order_item, data=request.data, partial=True)
-           serializer.is_valid(raise_exception=True)
-           serializer.save()
-           return Response({
-                'status': status.HTTP_200_OK,
-                'message': 'Order item Updated Successfully'  
-                },status=200)
-        except Order.DoesNotExist:
-            return Response({
-               'status': status.HTTP_404_NOT_FOUND,
-                'message': 'invalid Order_item_id',
-                },
-                status=404)
+# # update Order
+# class OrderItemUpdateView(GenericAPIView):
+#     serializer_class = OrderItemSerializer
+#     def patch(self, request, input):
+#         _id = input
+#         try:
+#            order_item = OrderItem.objects.get(order_item_id=_id)
+#            serializer = OrderSerializer(order_item, data=request.data, partial=True)
+#            serializer.is_valid(raise_exception=True)
+#            serializer.save()
+#            return Response({
+#                 'status': status.HTTP_200_OK,
+#                 'message': 'Order item Updated Successfully'  
+#                 },status=200)
+#         except Order.DoesNotExist:
+#             return Response({
+#                'status': status.HTTP_404_NOT_FOUND,
+#                 'message': 'invalid Order_item_id',
+#                 },
+#                 status=404)
 
 # get Order or get Order by id   
 class OrderView(APIView):
@@ -79,34 +79,34 @@ class OrderView(APIView):
         
 
 # delete Order
-class OrderDeleteView(APIView):
-    def delete(self, request, input):
-        try:
-            _id = input
-            order_item = OrderItem.objects.get(order_item_id=_id)
-            order_item.delete()
-            return Response({
-                'status': status.HTTP_200_OK,
-                'message': 'Order item Deleted Successfully'
-            },status=200)
-        except Order.DoesNotExist:
-            return Response({
-                'status': status.HTTP_404_NOT_FOUND,
-                'message': 'Invalid Order_item_id'
-            }, status=status.HTTP_404_NOT_FOUND)
+# class OrderDeleteView(APIView):
+#     def delete(self, request, input):
+#         try:
+#             _id = input
+#             order_item = OrderItem.objects.get(order_item_id=_id)
+#             order_item.delete()
+#             return Response({
+#                 'status': status.HTTP_200_OK,
+#                 'message': 'Order item Deleted Successfully'
+#             },status=200)
+#         except Order.DoesNotExist:
+#             return Response({
+#                 'status': status.HTTP_404_NOT_FOUND,
+#                 'message': 'Invalid Order_item_id'
+#             }, status=status.HTTP_404_NOT_FOUND)
 
 # order item add
-class OrderItemAddView(GenericAPIView):
-    serializer_class = OrderItemSerializer
-    def post(self, request,format=None):
-        serializer = OrderItemSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+# class OrderItemAddView(GenericAPIView):
+#     serializer_class = OrderItemSerializer
+#     def post(self, request,format=None):
+#         serializer = OrderItemSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
 
 
-        return Response({
-            'status':status.HTTP_201_CREATED,
-            "msg":'Order item added successfully',
-            'data':serializer.data
-        },status=201)
+#         return Response({
+#             'status':status.HTTP_201_CREATED,
+#             "msg":'Order item added successfully',
+#             'data':serializer.data
+#         },status=201)
 
