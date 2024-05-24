dc5b0dd09339b181fd4ff9dba6ef0c83278c1f20
Krishna Sadhu <Krishnas.bca2022@ssism.org>
2024-05-24 14:15:01 +0530

    Add Authorization in settings

--- StartBusiness/StartBusiness/settings.py
+++ StartBusiness/StartBusiness/settings.py
@@ -221,7 +221,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
       
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ),
 }
 
