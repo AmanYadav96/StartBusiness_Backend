826592e6ea2087cab21a8b1c9b1e4ffa35113cc6
Kuldeep Yadav <kuldeepy.bca2022@ssism.org>
2024-07-09 15:35:30 +0530

    create filter by price range , brand_name and category_name

--- StartBusiness/product/filter.py
+++ StartBusiness/product/filter.py
@@ -4,7 +4,11 @@ from product.models import Product
 
 class ProductFilter(FilterSet):
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    max_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
+    min_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
+    category_name = django_filters.CharFilter(field_name='category__category_name',lookup_expr='icontains')
+    brand_name = django_filters.CharFilter(field_name='brand__brand_name',lookup_expr='icontains') 
     class Meta:
         model = Product
-        fields = ['name','status','brand','category']
+        fields = ['name','status','brand','category','max_price','min_price','category_name','brand_name']
 
